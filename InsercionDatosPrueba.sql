f77906bbfece8a566e2f05a4b8616c6b4421b06c
tamateespinosa <tomas.amate@gmail.com>
2017-03-08 10:49:29 +0100

    entrega

---
+++ InsercionDatosPrueba.sql
@@ -0,0 +1,6 @@
+insert into cliente values (null,"Tomás","Amate Espinosa","tomas.amate@gmail.com");
+insert into cliente values (null,"Sergio","Peinado","sergio.peinado@gmail.com");
+insert into cliente values (null,"Paco","Perez Lopez","paco.perez@gmail.com");
+insert into producto values (null,"Leche","Puleva",25);
+insert into producto values (null,"Zumo","Don Simon",20);
+insert into producto values (null,"Chocolate","Nestle",50);
